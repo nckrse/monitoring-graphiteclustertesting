1c3c664078fe75e0f5fba7238110984ce4f2881b
Vagrant <reese@pythian.com>
2014-12-26 16:57:23 -0200

    Reordered classes, removed a class, and fixed puppet class error via manual LINT

--- nodes/mon.pp
+++ nodes/mon.pp
@@ -1,7 +1,5 @@
 node /^mon/ {
 
-# GRAPHITE
-
 include monit
 
 class {'graphitecluster::elasticsearch::base': }->
@@ -13,9 +11,7 @@ class {'graphitecluster::apache::config': }->
 class {'graphitecluster::graphite::base': }->
 class {'graphitecluster::grafana::base': }->
 class {'graphitecluster::apache::monit': }->
-class {'graphitecluster::graphite::monit': }->
-
-# HAPROXY
+class {'graphitecluster::graphite::monit': }
 
 class {'haproxycluster::keepalive::base': }->
 class {'haproxycluster::keepalive::config': }->
@@ -23,17 +19,10 @@ class {'haproxycluster::keepalive::config': }->
 class {'haproxycluster::haproxy::base': }->
 class {'haproxycluster::haproxy::admin': }->
 
-class {'haproxycluster::redishappy::config': } ->
-
 class {'haproxycluster::haproxy::graphiteweb': }->
 class {'haproxycluster::haproxy::graphiteinput': }->
 class {'haproxycluster::haproxy::elasticsearch': }->
 
-class {'haproxycluster::haproxy::members': }->
-
-# MONITORING
-
-class {'monitoring::graphite::client': }->
-class {'monitoring::graphite::client::monit': }
+class {'haproxycluster::haproxy::members': }
 
 }
